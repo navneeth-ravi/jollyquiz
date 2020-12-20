9f1db1747567a71cc8c0e478dbfffb6e49192ff0
navneeth-ravi <70211287+navneeth-ravi@users.noreply.github.com>
2020-12-21 01:45:48 +0530

    Update settings.py

--- testpress_quiz/settings.py
+++ testpress_quiz/settings.py
@@ -109,10 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT=os.path.join(BASE_DIR,"static")
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-static_dir,
-]
+
 
 LOGIN_URL='/quizz'
 django_heroku.settings(locals())
